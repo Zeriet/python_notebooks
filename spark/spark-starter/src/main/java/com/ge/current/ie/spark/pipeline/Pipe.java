f676ef0774796f2afdef7b2e0f1c3b4495cce044
Zeremariam Woldeab <zeremariam.woldeab@ge.com>
2017-05-05 01:05:54 -0700

    updates

--- spark/spark-starter/src/main/java/com/ge/current/ie/spark/pipeline/Pipe.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2016 GE. All Rights Reserved.
- * GE Confidential: Restricted Internal Distribution
- */
-package com.ge.current.ie.spark.pipeline;
-
-public interface Pipe<T, U> {
-    U process(T val);
-}
--- spark/spark-starter/src/main/java/com/ge/current/ie/spark/pipeline/Tap.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2016 GE. All Rights Reserved.
- * GE Confidential: Restricted Internal Distribution
- */
-package com.ge.current.ie.spark.pipeline;
-
-public interface Tap<T> {
-    void process(T data);
-}
